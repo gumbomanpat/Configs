26a461bddd34a8eac604fe7c139f00de71f02c48
Patirck Izzo <patrick.e.izzo@outlook.com>
2025-10-21 23:10:09 -0400

    new config files

---
+++ fish/config.fish
@@ -0,0 +1,22 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+#define the path  to my SSH key
+#set key-path /home/patrick/.ssh/github
+fastfetch
+starship init fish | source
+
+alias ls "eza -lahg --group-directories-first"
+alias .. "cd .."
+alias upd "yay -Syu"
+alias bpy "bpytop"
+alias add "git add -A"
+alias push "git push origin"
+alias commit "git commit -m"
+
+
+eval (ssh-agent -c)
+ssh-add  ~/.ssh/github
+
+
